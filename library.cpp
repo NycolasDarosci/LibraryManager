7c187f5e804d584876bd282ee2b0334467ce0a02
gabrieltakaki <gabitj@icloud.com>
2024-09-16 15:50:00 -0300

    refactor: makes the getByName and getBook method return a pointer instead of the whole document

--- library.cpp
+++ library.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "book.hpp"
 #include "user.hpp"
-#include "vector"
+#include "library.hpp"
 
 using namespace std;
 
-void borrowBook(vector<Books> &books_vector, vector<User> &users_vector) {
+void loanBook(vector<Book> &books_vector, vector<User> &users_vector) {
     string name, title, author;
     cin.ignore();
     
     cout << "Digite o nome do usuário que irá emprestar: ";
     getline(cin, name);
     
-    User& user = getUser(users_vector, name);
+    User* user = getByName(users_vector, name);
     
     cout << "Digite o título do livro: "; getline(cin, title);
     cout << "Digite o autor: "; getline(cin, author);
     
-    Books& book = getBook(books_vector, title, author);
+    Book* book = getBook(books_vector, title, author);
     
-    if (book.copies == 0) {
-        cout << "Sem o livro: " << book.title << " no estoque!";
+    if (book->copies == 0) {
+        cout << "Sem o livro: " << book->title << " no estoque!";
         //return;
     }
     
-    user.story_borrowed_books.append(book.title + " ");
-    user.current_borrowed_books = user.current_borrowed_books.append(book.title.append(", " + book.author));
-    user.balance++;
-    book.copies--;
+    user->story_borrowed_books.append(book->title + " ");
+    user->current_borrowed_books = user->current_borrowed_books.append(book->title.append(", " + book->author));
+    user->balance++;
+    book->copies--;
     
     cout << "Livro emprestado com sucesso!";
 }
