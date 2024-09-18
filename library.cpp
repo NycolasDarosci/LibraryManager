88ab7dbcd26f3b13ef716ff8f9704afe745fbc7a
gabrieltakaki <gabitj@icloud.com>
2024-09-17 22:26:26 -0300

    feat: loan_fine based on the borrowed_book.return_date of 1 week

--- library.cpp
+++ library.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <time.h>
+#include <ctime>
 
 #include "book.hpp"
 #include "user.hpp"
@@ -10,6 +10,8 @@
 using namespace std;
 
 void borrow_book(vector<Book> &books_vector, vector<User> &users_vector) {
+    time_t current_time = time(NULL);
+
     string name, title, author;
     cin.ignore();
 
@@ -27,13 +29,19 @@ void borrow_book(vector<Book> &books_vector, vector<User> &users_vector) {
     if (book->copies == 0) {
         cout << "Sem o livro: " << book->title << " no estoque!";
     } else {
+        // Define o tempo de uma semana em segundos (7 dias * 24 horas * 3600 segundos por hora).
+        const time_t one_week = 7 * 24 * 3600;
+        // Adiciona o tempo de uma semana no tempo atual (1 semana do momento do aluguel).
+        time_t future_time = current_time + one_week;
+
         user->story_borrowed_books.push_back(*book);
-        user->current_borrowed_books.push_back(*book);
+        user->current_borrowed_books.push_back((UserBorrowedBook){
+            .book = book,
+            .return_date = future_time
+        });
         user->balance++;
-        struct tm *local_time_date;
-        local_time_date = localtime(&user->return_book.tm_sec);
         book->copies--;
-        book->numberOfLoans++;
+        book->number_of_loans++;
 
         cout << "Livro emprestado com sucesso!";
     }
@@ -51,15 +59,15 @@ void loan_report(vector<Book> &books_vector, vector<User> &users_vector) {
 
     for (const Book& book : books_vector) {
         cout << "Título: " << book.title << endl;
-        cout << "Número de empréstimos: " << book.numberOfLoans << endl;
+        cout << "Número de empréstimos: " << book.number_of_loans << endl;
 
-        total_number_loans += book.numberOfLoans;
+        total_number_loans += book.number_of_loans;
 
-        if (book.numberOfLoans > most_popular_books[2].numberOfLoans) {
+        if (book.number_of_loans > most_popular_books[2].number_of_loans) {
             most_popular_books[2] = book;
 
             for (int i = 2; i > 0; i--) {
-                if (most_popular_books[i].numberOfLoans > most_popular_books[i - 1].numberOfLoans) {
+                if (most_popular_books[i].number_of_loans > most_popular_books[i - 1].number_of_loans) {
                     Book temp = most_popular_books[i];
                     most_popular_books[i] = most_popular_books[i - 1];
                     most_popular_books[i - 1] = temp;
@@ -109,17 +117,17 @@ void list_book_by_user(vector<User> &users_vector){
     SelectedUser selected_user = select_user(users_vector);
 
     if (selected_user.user->current_borrowed_books.size() == 0) {
-        cout << "\nUsuário não contém livros em empréstimo \n";
+        cout << "\nUsuário não contém livros em empréstimo\n";
         return;
     }
 
     for(int i = 0; i <= selected_user.user->current_borrowed_books.size() - 1; i++){
-        Book* current_borrowed_book = &selected_user.user->current_borrowed_books[i];
-        cout << "Título: " << current_borrowed_book->title << endl;
-        cout << "Autor: " << current_borrowed_book->author << endl;
-        cout << "Ano: " << current_borrowed_book->year << endl;
-        cout << "Cópias: " << current_borrowed_book->copies << endl;
-        cout << "Número de Cópias: " << current_borrowed_book->numberOfLoans << endl;
+        UserBorrowedBook* current_borrowed_book = &selected_user.user->current_borrowed_books[i];
+        cout << "\nTítulo: " << current_borrowed_book->book->title << endl;
+        cout << "Autor: " << current_borrowed_book->book->author << endl;
+        cout << "Ano: " << current_borrowed_book->book->year << endl;
+        cout << "Número de Cópias: " << current_borrowed_book->book->number_of_loans << endl;
+        cout << "Final do empréstimo: (1 semana após o aluguel) " << ctime(&current_borrowed_book->return_date) << endl;
     }
 }
 
@@ -131,7 +139,11 @@ void return_book(vector<Book> &books_vector, vector<User> &users_vector) {
         return;
     }
 
-    SelectedBook selected_book = select_book(selected_user.user->current_borrowed_books);
+    vector<Book> books;
+    for (const UserBorrowedBook &borrowedBook : selected_user.user->current_borrowed_books) {
+        books.push_back(*borrowedBook.book);
+    }
+    SelectedBook selected_book = select_book(books);
 
     Book* book = find(books_vector, selected_book.book->title, selected_book.book->author);
     if (book == nullptr) {
@@ -144,4 +156,28 @@ void return_book(vector<Book> &books_vector, vector<User> &users_vector) {
 
     cout << "\nDevolução do livro " << book->title << " realizada com sucesso!\n";
     cout << "Livro contém agora " << book->copies << " cópias.\n";
+}
+
+void loan_fine(vector<User> &users_vector) {
+    time_t current_time = time(NULL);
+
+    if (users_vector.size() == 0) {
+        cout << "Sua biblioteca ainda não consta usuários no sistema.\n";
+    } else {
+        for (User &user : users_vector) {
+
+            for (const UserBorrowedBook &borrowed_book : user.current_borrowed_books) {
+                if (borrowed_book.return_date < current_time) {
+                    cout << "\nO período de empréstimo do usuário " << user.name << " para o livro " << borrowed_book.book->title << " expirou!\n";
+                    cout << "Expirou em: " << ctime(&borrowed_book.return_date);
+                    cout << "Adicionada uma multa de R$ 20.\n";
+                    user.balance -= 20;
+                } else {
+                    cout << "\nO período de empréstimo do usuário " << user.name << " para o livro " << borrowed_book.book->title << " está em dia.\n";
+                    cout << "Irá expirar em: " << ctime(&borrowed_book.return_date) << endl;
+                }
+            }
+
+        }
+    }
 }
